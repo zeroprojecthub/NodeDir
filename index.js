8c0c01d451e15f02ebff76e6e632bcab55261ba2
zeroprojcthub <zeroprojecthub@gmail.com>
2023-09-16 16:25:27 +0530

    instagram simple template implemented

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
- const express = require('express'); 
- const app = express();
- const path=require("path");
-//  const  ejs=require('ejs');
+const express = require('express');
+const app = express();
+const path = require("path");
+const ejs = require('ejs');
 
 
+const port = 3000;
+app.set("view engine", "views");
+app.set("views", path.join(__dirname, "/views"));
 
- const port=3000;
-app.set("view engine","views");
-app.set("views",path.join(__dirname,"/views"));
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
    // res.send("Home page ");
-   let random = Math.floor(Math.random()*6+1);
-   res.render("home.ejs",{ num : random });
+   let random = Math.floor(Math.random() * 6 + 1);
+   res.render("home.ejs", { num: random });
 });
- app.listen(port,()=>{
-    console.log(`app is listing on port${port}`);
- })
+
+app.get("/ig/:username", (req, res) => {
+console.log(req.params);
+   let { username } = req.params;
+   res.render("instagram.ejs", { username });
+})
+
+app.listen(port, () => {
+   console.log(`app is listing on port${port}`);
+})
